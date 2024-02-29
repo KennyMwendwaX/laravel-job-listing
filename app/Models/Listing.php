da2fd528591b38c20027f7f9e80d61c86be03949
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-02-29 09:28:09 +0300

    feat: create Listings Model

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,30 +2,10 @@
 
 namespace App\Models;
 
-class Listing {
-    public static function all() {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Post 1', 
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus nihil eaque dicta omnis voluptates aliquid, quo unde vitae molestiae rerum.'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Post 2',
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus nihil eaque dicta omnis voluptates aliquid, quo unde vitae molestiae rerum.'
-            ]
-            ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id) {
-        $listings = self::all();
-
-        foreach ($listings as $listing) {
-            if($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
-
--- database/migrations/2024_02_29_060502_create_listings_table.php
+++ database/migrations/2024_02_29_060502_create_listings_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('location');
             $table->string('email');
             $table->string('website');
-            $table->string('description');
+            $table->longText('description');
             $table->timestamps();
         });
     }
